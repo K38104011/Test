01da1f36c2fcd5cd6702888128d650536d3247b1
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-12-17 10:16:43 +0700

    Add project files.

---
+++ CountFor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var power = 2;
+            do
+            {
+                var inputSize = Math.Pow(10, power);
+                var countFor = new CountFor((int) inputSize);
+                Console.WriteLine(countFor.Result);
+                power++;
+            } while (power < 7);
+            Console.ReadKey();
+        }
+    }
+
+    class CountFor
+    {
+        public int Result { get; }
+
+        public CountFor(int size)
+        {
+            var result = 0;
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = i + 1; j <= size; j++)
+                {
+                    result++;
+                }
+            }
+            Result = result;
+        }
+    }
+}
---
+++ MaxSubArraySum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MaxSubArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] {-1, 2, 4, -3, 5, 2, -5, 2};
+
+            var sumResult = 0;
+            var subArraySize = 1;
+            do
+            {
+                for (var beginIndex = 0; beginIndex < arr.Length; beginIndex++)
+                {
+                    if (beginIndex + subArraySize >= arr.Length)
+                    {
+                        continue;
+                    }
+                    var subArraySum = SumSubArray(beginIndex, subArraySize, arr);
+                    if (subArraySum > sumResult)
+                    {
+                        sumResult = subArraySum;
+                    }
+                }
+                subArraySize++;
+            } while (subArraySize <= arr.Length);
+
+            Console.WriteLine(sumResult);
+            Console.ReadKey();
+        }
+
+        private static int SumSubArray(int beginIndex, int sizeSubArray, int[] arr)
+        {
+            var sum = 0;
+            for (var i = 0; i < sizeSubArray; i++)
+            {
+                sum += arr[beginIndex + i];
+            }
+            return sum;
+        }
+    }
+}
